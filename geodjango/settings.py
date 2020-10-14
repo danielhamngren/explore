c8660816dd21091f47b3f2828034e0f85a8c3698
danielhamngren <daniel.hamngren@gmail.com>
2020-10-14 15:33:08 +0200

    Add settings for smtp mail
    An email service was needed in order to send password resets.

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -94,8 +94,19 @@ DATABASES = {
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# EMAIL_FILE_PATH = str(BASE_DIR.joinpath('sent_emails'))
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+if os.getenv("EMAIL_USE_TLS") == "True":
+    EMAIL_USE_TLS = True
+else:
+    EMAIL_USE_TLS = False
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
